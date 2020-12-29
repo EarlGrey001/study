1bcbe45b2196c4fec6aa646ff6f85ae7c002879e
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-12-29 22:03:51 +0900

    Update paiza.rb

--- paiza.rb
+++ paiza.rb
@@ -11,3 +11,14 @@ inputs = input.split(" ")
 a = inputs[0].to_i
 b = inputs[1].to_i
 puts a + b
+
+最小値
+
+n_1 = gets.to_i
+n_2 = gets.to_i
+n_3 = gets.to_i
+n_4 = gets.to_i
+n_5 = gets.to_i
+
+number = [n_1, n_2, n_3, n_4, n_5 ]
+puts number.min
