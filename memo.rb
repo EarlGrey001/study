fd30b0dfa32afc5893f922effa06a2d9c100930d
earlgrey01 <sakayori0817@yahoo.co.jp>
2021-01-13 16:36:21 +0900

    Update memo.rb

--- memo.rb
+++ memo.rb
@@ -3,6 +3,11 @@ ruby on rails
 HTML/CSS
 javascript
 
+input = gets
+puts input
+
+
+
 #input_lines = 2という１行目のデータが入る
 input_lines = gets.to_i
 
@@ -15,11 +20,17 @@ input_lines.times {
 }
 
 
-getsメソッド：　入力を一行ごとに「文字列」で受け取る。
-chompメソッド：　文字列の改行を取り除く。
-to_iメソッド：　「文字列」を「数」に変換する。
-splitメソッド：　文字列を分割し、配列に格納する。
+getsメソッド： 入力を一行ごとに「文字列」で受け取る。
+chompメソッド： 文字列の改行を取り除く。
+to_iメソッド： 「文字列」を「数」に変換する。
+splitメソッド： 文字列を分割し、配列に格納する。
 
 printメソッド
 putsメソッド
 
+input_1 = gets
+input_2 = gets
+input_3 = gets
+
+list = [input_1,input_2,input_3]
+puts list
