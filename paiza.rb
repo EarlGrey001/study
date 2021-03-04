a11d8c7b6a601541eb4f7a3de51a99fc17bffaea
earlgrey01 <sakayori0817@yahoo.co.jp>
2021-03-04 19:07:45 +0900

    Update paiza.rb

--- paiza.rb
+++ paiza.rb
@@ -89,6 +89,22 @@ era += yy == 1 ? '元' : yy.to_s
 
 puts "#{era}年#{m}月#{d}日"
 
+#月の日数
+def is_leap?(y)
+  (y % 400).zero? || (y % 100).nonzero? && (y % 4).zero?
+end
+
+y, m = gets.split.map(&:to_i)
+
+puts case m
+     when 2
+       is_leap?(y) ? 29 : 28
+     when 4, 6, 9, 11
+       30
+     else
+       31
+      end
+
 #
 
 input = gets  # 標準入力の取得
