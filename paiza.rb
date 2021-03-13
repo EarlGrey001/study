61ee0549d13b330c2a25b621abc48ddbb288dfb9
earlgrey01 <sakayori0817@yahoo.co.jp>
2021-03-13 23:10:39 +0900

    Update paiza.rb

--- paiza.rb
+++ paiza.rb
@@ -1,11 +1,3 @@
-#掛け算
-
-input = gets.chomp.to_i
-input2 = gets.chomp.to_i
-puts input * input2
-
-
-
 #足し算
 
 input = gets.chomp
@@ -13,6 +5,14 @@ inputs = input.split(" ")
 a = inputs[0].to_i
 b = inputs[1].to_i
 puts a + b
+
+
+#掛け算
+
+input = gets.chomp.to_i
+input2 = gets.chomp.to_i
+puts input * input2
+
 
 
 #最小値
